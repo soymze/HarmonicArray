928cfe3793106adfd1bb7c1627c6ffd501d2714e
soymze <soylmze@gmail.com>
2024-02-16 12:39:52 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numbers = {1, 2, 3, 4, 5};
+        double harmonicAvg = 0.0;
+        for (int i = 0; i < numbers.length; i++) {
+            harmonicAvg +=   1.0 /(double)numbers[i];
+        }
+
+        System.out.println( (double) numbers.length/harmonicAvg);
     }
 }
+
